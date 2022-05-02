ce5a23e9f551ae82b2fe3d68bf9cbd9a23982cc4
Soumya <soumamoulik@gmail.com>
2022-05-02 17:28:53 +0530

    update

--- src/components/ProtextedRoute.jsx
+++ src/components/ProtextedRoute.jsx
@@ -1,9 +1,9 @@
 // /orders and /neworder are protected routes
 
-import { useSelector } from "react-redux";
+
 import { Navigate } from "react-router-dom";
 export const ProtectedRoute = ({ children }) => {
-    const status = useSelector(store => store.isLoggedIn);
+  
     const isAuth = true
     if(!isAuth){
         return < Navigate to="/login" />
